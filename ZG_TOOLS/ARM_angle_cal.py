2e321a51e3a22b622cc6aca1fc4af2c1c66c17c5
FB99 <34882872+FB99@users.noreply.github.com>
2019-10-29 09:49:53 +0800

    Update ARM_angle_cal.py

--- ZG_TOOLS/ARM_angle_cal.py
+++ ZG_TOOLS/ARM_angle_cal.py
@@ -7,17 +7,17 @@ D = 165
 H = 0
 p1 = np.array([0, -D, -H])
 orient = np.array([-1, 0, 0])
-
+#坐标转换
 def Rx(theta):
     return np.array([[1,      0,              0       ],
                      [0, np.cos(theta), -np.sin(theta)],
                      [0, np.sin(theta),  np.cos(theta)]])
-
+#坐标转换
 def Ry(theta):
     return np.array([[ np.cos(theta), 0, np.sin(theta)],
                      [      0,        1,      0       ],
                      [-np.sin(theta), 0, np.cos(theta)]])
- 
+ #坐标转换
 def Rz(theta):
     return np.array([[np.cos(theta), -np.sin(theta), 0], 
                      [np.sin(theta),  np.cos(theta), 0],
@@ -26,9 +26,10 @@ def Rz(theta):
 def cal_R(alpha, beta, gamma):
     return np.dot(np.dot(Rx(alpha), Ry(beta)), Rz(gamma))
 
+#弧度转角度
 def arc2C(value):
   return value / np.pi * 180
-
+#计算前三个关节转角
 def get_q123(q4, R4):
     R = np.dot(R4, np.transpose(Rx(q4)))
     print(R)
@@ -94,15 +95,9 @@ def get_bag(p6):
     #### check:
     Rq3 = np.dot(np.dot(Ry(q1), Rx(q2)), Rz(q3))
     Rq4 = np.dot(np.dot(np.dot(Ry(q1), Rx(q2)), Rz(q3)), Rx(q4))
-    print("RQ3:"+str([Rq3[0][2],Rq3[1][2],Rq3[2][2]]))
-    print("RQ4:"+str([Rq4[0][2],Rq4[1][2],Rq4[2][2]]))
+
     result = p1-np.array([Rq3[0][2],Rq3[1][2],Rq3[2][2]])*170-np.array([Rq4[0][2],Rq4[1][2],Rq4[2][2]])*170
-    #result = p1 + [-0.095,0.061521,-0.07457] * 170 +[-0.243, 0.617,-0.7481]*170
-    print("actual destination point:")
-    print(result)
-    print("######################")
-    # print(R4)
-    # print(R)
+
     q5 = np.arctan(-R4[2, 0] / R4[2, 1])
     R5 = np.dot(R4, Rz(q5))
     q6 = np.arctan(-R5[0, 1] / R5[0, 2])
@@ -113,10 +108,10 @@ def get_bag(p6):
             np.clip(transq2(0 - 1.11*arc2C(q3)),0,200), np.clip(transq2(140 - 1.11*arc2C(q4)),10,200),
              180 - 1.11*arc2C(q5), 90 + 1.11*arc2C(q6)]
 
-#q1 range[-10,200] 向前角度增大   初始值40  145 = 90 degree naturall
-#q2 range[10,160] 向内角度增大  初始值140  30 = 90 degeree naturally
-#q3 range[-20,200] 从上往下看，顺时针方向角度增大 初始值90  -10, 190 = 90 degree
-#q4 range[10，200]   向后角度增大  初始值140   40 = 90 degree
+#q1 range[-10,200] 向前角度增大   初始值40 
+#q2 range[10,160] 向内角度增大  初始值140  
+#q3 range[-20,200] 从上往下看，顺时针方向角度增大 初始值90 
+#q4 range[10，200]   向后角度增大  初始值140   
 #q5 range[-20,200] 从上往下看，顺时针方向角度增大   初始值 90
 #q6 range[-20,150] 往内角度增大  初始值 90
 
@@ -128,10 +123,6 @@ def transq2(q):
     return q
 
 def cal_roll_agl(p6, R6):
-    '''
-    p6: 涓栫晫鍧愭爣绯讳腑鐨勭偣
-    R6: 鐩爣鍧愭爣绯荤殑鍩哄湪涓栫晫鍧愭爣绯讳腑鐨勮〃绀�
-    '''
     r = p1 - p6
     C = np.linalg.norm(r)
     q4 = np.arccos((L1 ** 2 + L2 **2 - C ** 2) / 2 / L1 / L2) - np.pi
@@ -151,29 +142,6 @@ def cal_roll_agl(p6, R6):
 
 
 if __name__ == '__main__':
-    
-    # p = np.array([200, 0, -30])
-    # R = np.array([[-np.sqrt(2) / 2, -2 / 3, np.sqrt(3) / 3],
-    #       [0, 1 / 3, np.sqrt(3) / 3],
-    #       [np.sqrt(2) / 2, -2 / 3, np.sqrt(3) / 3]])
-    # ans = cal_roll_agl(p, R)
-    # print(ans)
-
-    # p[0] += 30
-    # ans = cal_roll_agl(p, R)
-    # print(ans)
-
-    # R *= cal_R(np.pi / 6, 0, 0)
-    # ans = cal_roll_agl(p, R)
-    # print(ans)
-
-    # p[1] -= 20
-    # ans = cal_roll_agl(p, R)
-    # print(ans)
-
-    # p = np.array([200, -200, -30])
-    # angles = get_bag(p)
-    # print(angles)
 
     p = np.array([170, 5, 0])
     R = np.transpose(np.array([[0, 0, 1],
