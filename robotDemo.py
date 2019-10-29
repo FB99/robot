b05a8357959c3f3f6887dac02720209d4b25e55d
FB99 <34882872+FB99@users.noreply.github.com>
2019-10-29 09:36:01 +0800

    Update robotDemo.py

--- robotDemo.py
+++ robotDemo.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 # encoding: utf-8
-'''
-@author: Xinming Hou
-@license: (C) Copyright 2017-2018, CCNT of Zhejiang Unversity.
-@contact: houxinming.chn@foxmail.com
-@file: robotDemo.py
-@time: 2018/10/10 5:56 PM
-@description:
-'''
 
 import sys
 import os
