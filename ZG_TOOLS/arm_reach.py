91a509b973d4b017bfa3eb50baada985dbbe936d
FB99 <34882872+FB99@users.noreply.github.com>
2019-11-13 17:05:07 +0800

    Update arm_reach.py

--- ZG_TOOLS/arm_reach.py
+++ ZG_TOOLS/arm_reach.py
@@ -7,9 +7,13 @@
 3，利用坐标转换及长度计算 算出当前抓手的手指位置
 4，计算当前位置和目标点位置的欧式距离，作为reward，返回给主程序
 """
+import numpy as np
+from PIL import Image
+import shutil
 
 class Arm_Reach():
     def angle2tip
-    def get_distance
+    def get_distance(self):
+        
     def xyz2trz
     def trz2xyz
