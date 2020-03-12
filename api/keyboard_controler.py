ecfa329184593c5a05c5d47b5c8e5d1ddfdce815
FB99 <34882872+FB99@users.noreply.github.com>
2020-03-12 15:30:55 +0800

    Update keyboard_controler.py

--- api/keyboard_controler.py
+++ api/keyboard_controler.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-'''
-@author: Xinming Hou
-@license: (C) Copyright 2017-2018, CCNT of Zhejiang Unversity.
-@contact: houxinming.chn@foxmail.com
-@file: keyboard_controler.py
-@time: 2018/3/26 下午6:51
-@description:
 
-    old version
-
-'''
 
 from .robot_interface import *
 from .robot_parament import *
